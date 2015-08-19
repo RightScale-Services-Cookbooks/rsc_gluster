7ad2b8dfa554ecf45162cd4a8bfad149b2b9c88d
tusharshahrs <tushar.shah@rightscale.com>
2015-08-19 16:26:02 -0400

    redid the tags with tag_results

--- recipes/server-peer-probe.rb
+++ recipes/server-peer-probe.rb
@@ -8,12 +8,11 @@ end
 
 include_recipe "machine_tag::default"
 
-glusterfs_peers = Array.new
-tags = tag_search(node, "gluster:server=true").first
+glusterfs_peers = []
+tags_results = tag_search(node, "gluster:server=true").first
 Chef::Log.info "tags:" + tags.inspect
 
-glusterfs_peers = Array.new
-tags.each do |itemlist|
+tags_results.each do |itemlist|
   block do
     glusterfs_peers << itemlist["server:private_ip_0"].first.split('=').last
     Chef::Log.info " Here are the ips addresses that we got from ruby block of gluster peer " + itemlist["server:private_ip_0"].first.split('=').last
@@ -27,4 +26,4 @@ node.override['gluster']['peers'] = #{glusterfs_peers}
 
 Chef::Log.info "Gluster Peers #{node['gluster']['peers']}"
 
-include_recipe "gluster::server-peer-probe"
+#include_recipe "gluster::server-peer-probe"
