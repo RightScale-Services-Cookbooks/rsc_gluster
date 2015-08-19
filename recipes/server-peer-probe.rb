8ca576a6c844fdd8ac266aaedd1a126948133693
Richard Shade <rshade@rightscale.com>
2015-08-19 12:57:58 -0500

    adding tagging

---
+++ recipes/server-peer-probe.rb
@@ -0,0 +1,21 @@
+marker "recipe_start_rightscale" do
+  template "rightscale_audit_entry.erb"
+end
+
+class Chef::Resource::RubyBlock
+  include Chef::MachineTagHelper
+end
+
+include_recipe "machine_tag::default"
+
+glusterfs_peers = Array.new
+r=ruby_block "find server ip" do
+  block do
+    tags = tag_search(node, "").first
+    Chef::Log.info "tags: #{tags.inspect}"
+    glusterfs_ip =  tags["server:private_ip_0"].first.value
+  end
+end
+r.run_action(:create)
+
+node.set['gluster']['peers'] = glusterfs_peers
--- recipes/server.rb
+++ recipes/server.rb
@@ -4,6 +4,8 @@ end
 
 include_recipe "gluster::server"
 
+include_recipe "rightscale_tag::default"
+
 machine_tag "gluster:server=true" do
   action :create
 end
