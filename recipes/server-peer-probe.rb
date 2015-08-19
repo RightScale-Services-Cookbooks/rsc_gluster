340aa1a59f748476130950749aaebaa87014a322
Richard Shade <rshade@rightscale.com>
2015-08-19 16:29:55 -0500

    adding ip_address variable, and more logging

--- recipes/server-peer-probe.rb
+++ recipes/server-peer-probe.rb
@@ -8,19 +8,21 @@ end
 
 include_recipe "machine_tag::default"
 
+Chef::Log.info "Beginning gluster search"
 glusterfs_peers = []
 tags_results = tag_search(node, "gluster:server=true")
 Chef::Log.info "tags:" + tags_results.inspect
 
 tags_results.each do |itemlist|
-    Chef::Log.info " Here are the ips addresses that we got from ruby block of gluster peer " + itemlist["server:private_ip_0"].first.split('=').last
-    glusterfs_peers << itemlist["server:private_ip_0"].first.split('=').last
+    ip_address = itemlist["server:private_ip_0"].first.split('=').last
+    Chef::Log.info " Here are the ips addresses that we got from ruby block of gluster peer: #{ip_address}"
+    glusterfs_peers << ip_address
 end
 
-#Chef::Log.info "found peers #{glusterfs_peers}"
+Chef::Log.info "found peers #{glusterfs_peers}"
 
-#node.override['gluster']['peers'] = #{glusterfs_peers}
+node.override['gluster']['peers'] = glusterfs_peers
 
-#Chef::Log.info "Gluster Peers #{node['gluster']['peers']}"
+Chef::Log.info "Gluster Peers #{node['gluster']['peers']}"
 
 #include_recipe "gluster::server-peer-probe"
