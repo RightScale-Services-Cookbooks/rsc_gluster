ab9b28b258fe192fa8f0dc651fb4954bc22886b0
Richard Shade <rshade@rightscale.com>
2015-08-24 14:47:11 -0500

    cleaning up a little rubocop

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,11 +1,11 @@
-marker "recipe_start_rightscale" do
-  template "rightscale_audit_entry.erb"
+marker 'recipe_start_rightscale' do
+  template 'rightscale_audit_entry.erb'
 end
 
-include_recipe "gluster::server"
+include_recipe 'gluster::server'
 
-include_recipe "rightscale_tag::default"
+include_recipe 'rightscale_tag::default'
 
-machine_tag "gluster:server=true" do
+machine_tag 'gluster:server=true' do
   action :create
 end
