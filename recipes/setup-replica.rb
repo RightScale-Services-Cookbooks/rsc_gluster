ff34b99dd13bba55e4020f8ae3158d3b7cde5769
Richard Shade <rshade@rightscale.com>
2015-08-26 10:12:18 -0500

    fixing syntax error

--- recipes/setup-replica.rb
+++ recipes/setup-replica.rb
@@ -32,8 +32,9 @@ include_recipe 'gluster::setup-replica'
 rsc_remote_recipe "attach local client" do
   recipe "gluster::client"
   recipient_tags "gluster:server=true",
-  attributes { 'gluster/client/mount/point' => '/mnt/gluster',
-    'gluster/peers' => node['gluster']['peers']
-  }
+  attributes( {
+  'gluster/client/mount/point' => '/mnt/gluster',
+  'gluster/peers' => node['gluster']['peers']
+} )
   action :run
 end
