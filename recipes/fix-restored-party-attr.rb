0b909730577b4a10b0e6053f3abe946d2f3830a4
Richard Shade <rshade@rightscale.com>
2015-08-27 14:57:24 -0500

    replacing log with Chef::Log.info

--- recipes/fix-restored-party-attr.rb
+++ recipes/fix-restored-party-attr.rb
@@ -2,12 +2,12 @@ ruby_block "config attributes" do
   block do
     EXPORT_DIR=node['gluster']['brick']['path']
     require 'mixlib/shellout'
-    log "doing attribute set"
+    Chef::Log.info "doing attribute set"
     attr1=Mixlib::ShellOut.new("setfattr -x trusted.glusterfs.volume-id #{EXPORT_DIR}").run_command
-    log "setfattr -x trusted.glusterfs.volume-id #{EXPORT_DIR} STDOUT: #{attr1.stdout} STDERR: #{attr1.stderr}"
+    Chef::Log.info "setfattr -x trusted.glusterfs.volume-id #{EXPORT_DIR} STDOUT: #{attr1.stdout} STDERR: #{attr1.stderr}"
     attr2=Mixlib::ShellOut.new("setfattr -x trusted.gfid #{EXPORT_DIR}").run_command
-    log "setfattr -x trusted.gfid STDOUT: #{attr2.stdout} STDERR: #{attr2.stderr}"
+    Chef::Log.info "setfattr -x trusted.gfid STDOUT: #{attr2.stdout} STDERR: #{attr2.stderr}"
     attr3=Mixlib::ShellOut.new("rm -fr #{EXPORT_DIR}/.glusterfs").run_command
-    log "rm -fr #{EXPORT_DIR}/.glusterfs STDOUT: #{attr3.stdout} STDERR: #{attr3.stderr}"
+    Chef::Log.info "rm -fr #{EXPORT_DIR}/.glusterfs STDOUT: #{attr3.stdout} STDERR: #{attr3.stderr}"
   end
 end
